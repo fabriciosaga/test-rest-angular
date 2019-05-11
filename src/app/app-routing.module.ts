e48220da6d5a56f54a3e05da583e92684c350034
fabricio <fabricio.sanchez.work@gmail.com>
2019-05-11 18:24:19 -0400

    Adding some component to interchange elements between rest services

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { UsersComponent } from './users/users.component';
+import { UserDetailComponent } from './user-detail/user-detail.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "users", component:UsersComponent},
+  {path: "user", component:UserDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/user-service.service.ts
+++ src/app/user-service.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { UserItem } from './user-item';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserServiceService {
 
-  constructor() { }
+  constructor(private httpClient:HttpClient) { 
+
+  }
+
+  getUsers() {
+    return this.httpClient.get<UserItem[]>("http://localhost:8030/users");
+  }
+
+  getUser(id:number) {
+    return this.httpClient.get<UserItem>("http://localhost:8030/user/" + id);
+  }
 }
