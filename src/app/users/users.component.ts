10828a0f8b8eba3b88b00825ad73908dc28b7b24
fabricio <fabricio.sanchez.work@gmail.com>
2019-05-11 18:33:43 -0400

    Adding information about two services

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserServiceService } from '../user-service.service';
+import { UserItem } from '../user-item';
+
 
 @Component({
   selector: 'app-users',
@@ -7,9 +10,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  users:UserItem[];
+  user:UserItem;
+  
+  constructor(private userService:UserServiceService) { }
 
   ngOnInit() {
+    this.userService.getUsers().subscribe(
+      data => this.users = data,
+      error => console.log(error)
+    );
   }
 
+  showDetail(id:number){
+    this.userService.getUser(id).subscribe(
+        data => this.user=data,
+        error => console.log(error)
+      );
+  }
 }
